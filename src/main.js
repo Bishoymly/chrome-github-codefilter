a1bf38e0528fea4020777283d6b61ff72d0bb1ae
Bishoy Labib <bishoymamdouh@hotmail.com>
2017-07-22 20:57:24 +0200

    Added filter controls on page

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ var enableFilter = true;
 function refreshFiles()
 {
     var count = 0;
+    
+    //Hide files that are not .cs
     $(".file-header a.link-gray-dark").each(function() {
         if(enableFilter){
             if($(this).attr("title").endsWith(".cs") == false || $(this).attr("title").endsWith(".Designer.cs")){
@@ -16,12 +18,41 @@ function refreshFiles()
             count++
         }
     });
-    $(".blob-num.blob-num-deletion.js-linkable-line-number").parent().hide();
+
+    //Hide deleted code lines
+    if(enableFilter){
+        $(".blob-num.blob-num-deletion.js-linkable-line-number").parent().hide();
+    } else {
+        $(".blob-num.blob-num-deletion.js-linkable-line-number").parent().show();
+    }
+
     $("button.btn-link.muted-link.select-menu-button.js-menu-target").html("<strong>"+count+" files </strong>");
 }
 
 $(document).ready(function(){
+    
+    $("body").append('<div class="diffbar-item" style="position:fixed;right:5px;top:5px">'
+                    +'<div class="BtnGroup">'
+                    +'<button id="codefilter" type="button" class="btn btn-sm btn-outline BtnGroup-item tooltipped tooltipped-s" disabled="">Filter</button>'
+                    +'<button id="viewall" type="button" class="btn btn-sm btn-outline BtnGroup-item tooltipped tooltipped-s">All</button>'
+                    +'</div>'
+                    +'</div>');
+
     $(".diff-view.commentable").bind("DOMSubtreeModified", refreshFiles);
     refreshFiles();
+
+    $("#codefilter").click(function(){
+        enableFilter = true;
+        refreshFiles();
+        $("#codefilter").prop('disabled', true);
+        $("#viewall").prop('disabled', false);
+    });
+
+    $("#viewall").click(function(){
+        enableFilter = false;
+        refreshFiles();
+        $("#codefilter").prop('disabled', false);
+        $("#viewall").prop('disabled', true);
+    });
 });
 
